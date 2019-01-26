0105b2a00bb964b3eb242fa95f1a378adeea68c0
einat <beeineihamitbonen@gmail.com>
2019-01-26 21:17:51 +0200

    First version

--- src/QueueNStack/Queue.java
+++ src/QueueNStack/Queue.java
@@ -3,48 +3,49 @@ import unit4.collectionsLib.Node;
 
 public class Queue<T>
 {
-        public Node<T> first;
-        public Node<T> last;
+    public Node<T> first;
+    public Node<T> last;
 
 
-        public Queue()
-        {
-                this.first = null;
-                this.last = null;
-         }
+    public Queue()
+    {
+        this.first = null;
+        this.last = null;
+    }
 
-        public void insert(T x)
-         {
-                Node<T> temp = new Node<T>(x);
-                if (first == null)
-                        first = temp;
-                else
-                        last.setNext(temp);
-                last = temp;
-        }
+    public void insert(T x)
+    {
+        Node<T> temp = new Node<T>(x);
+        if (first == null)
+            first = temp;
+        else
+            last.setNext(temp);
+        last = temp;
+    }
 
-          public T remove()
-         {
-                 T x =  first.getValue();    
-                 first = first.getNext();
-                 if (first == null)
-                          last = null;
-                 return  x;
-        }        
+    public T remove()
+    {
+        T x =  first.getValue();
+        first = first.getNext();
+        if (first == null)
+            last = null;
+        return  x;
+    }
 
-        public T top()
-         {
-                 return  first.getValue();
-         }
+    public T top()
+    {
+        return  first.getValue();
+    }
 
-        public boolean isEmpty()
-        {             
-                 return first == null;                 
-        }        
+    public boolean isEmpty()
+    {
+        return first == null;
+    }
 
-         public String toString()
-        {
-              String temp = first.toString();
-              return "head -> " + temp.substring(0, temp.length()-4) + " end";     
-        }   
+    public String toString()
+    {
+        String temp = first.toString();
+        return "head -> " + temp.substring(0, temp.length()-4) + " end";
+    }
 }
+
